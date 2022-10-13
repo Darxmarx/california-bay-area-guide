1cb26a110d9bb2e1dac5e7705700be580fd95a38
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-10-13 10:44:53 -0700

    Connected the abreviatin function with the fetch, so the system will get the user full name, it will abbreviate the station name, and add it to the api search to get a response

--- assets/transport/transport.js
+++ assets/transport/transport.js
@@ -11,7 +11,7 @@ function fetchBart (station) {
     var apiKey = "MW9S-E7SL-26DU-VV8V&json=y";
 
     // fetch("https://api.bart.gov/api/etd.aspx?cmd=etd&orig="+ station + "&key=" + apiKey)
-    fetch("https://api.bart.gov/api/etd.aspx?cmd=etd&orig=RICH&key=MW9S-E7SL-26DU-VV8V&json=y")
+    fetch("https://api.bart.gov/api/etd.aspx?cmd=etd&orig="+station+"&key=MW9S-E7SL-26DU-VV8V&json=y")
 
     .then(function (response) {
         console.log(response)
@@ -25,8 +25,9 @@ function fetchBart (station) {
       
 }
 
-fetchBart();
+// fetchBart();
 
+// Function to compare the full station name and the Abbreviation name
 function setAbbreviation(station){
   var stationAbbr = "";
   for (var i = 0; i < stationFullName.length; i++){
@@ -36,68 +37,17 @@ function setAbbreviation(station){
     
   }
   console.log("This is the Statoin Abbreviation: " + stationAbbr);
-  return stationAbbr;
+  fetchBart(stationAbbr);
 }
 
-setAbbreviation("Richmond");
+setAbbreviation("Millbrae");
 
 
 // Create a autofill menu, so the user can chose what is the station they want, the reason we are creating in this way as the dart require the station name abreviation.
 $(function() {
-    var availableStation = [
-        "12th St. Oakland City Center",
-        "16th St. Mission (SF)",
-        "19th St. Oakland",
-        "24th St. Mission (SF)",
-        "Ashby (Berkeley)",
-        "Antioch",
-        "Balboa Park (SF)",
-        "Bay Fair (San Leandro)",
-        "Berryessa / North San Jose",
-        "Castro Valley",
-        "Civic Center (SF)",
-        "Coliseum",
-        "Colma",
-        "Concord",
-        "Daly City",
-        "Downtown Berkeley",
-        "Dublin/Pleasanton",
-        "El Cerrito del Norte",
-        "El Cerrito Plaza",
-        "Embarcadero (SF)",
-        "Fremont",
-        "Fruitvale (Oakland)",
-        "Glen Park (SF)",
-        "Hayward",
-        "Lafayette",
-        "Lake Merritt (Oakland)",
-        "MacArthur (Oakland)",
-        "Millbrae",
-        "Milpitas",
-        "Montgomery St. (SF)",
-        "North Berkeley",
-        "North Concord/Martinez",
-        "Oakland Int'l Airport",
-        "Orinda",
-        "Pittsburg/Bay Point",
-        "Pittsburg Center",
-        "Pleasant Hill",
-        "Powell St. (SF)",
-        "Richmond",
-        "Rockridge (Oakland)",
-        "San Bruno",
-        "San Francisco Int'l Airport",
-        "San Leandro",
-        "South Hayward",
-        "South San Francisco",
-        "Union City",
-        "Warm Springs/South Fremont",
-        "Walnut Creek",
-        "West Dublin",
-        "West Oakland"
-    ];
+  var staionAvailable = stationFullName;
     $("#station").autocomplete({
-      source: availableStation
+      source: staionAvailable
     });
   } );
 
