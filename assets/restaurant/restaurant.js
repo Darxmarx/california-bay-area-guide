e6468018d0a8b96a7566e6d8ec31935e787a92b6
Darx <mcrivabode@gmail.com>
2022-10-13 14:27:03 -0700

    IT FINALLY RESPONDS OH MY GOODNESS

--- assets/restaurant/restaurant.js
+++ assets/restaurant/restaurant.js
@@ -1,7 +1,7 @@
 //personal API key, 5,000 API calls per day
-const apiKey = "3pOEjNFRg7sRIZM6MAdUVDCecyWU-vdaV8QdqxD_WyHlIcsykEO1FknqGAuWwaBAJmqMShjlAbYVMaK2xg1K-1zBEMBUEjM96x7yJb6ykezbVh46jSWRwwhKMblFY3Yx"
+var apiKey = "3pOEjNFRg7sRIZM6MAdUVDCecyWU-vdaV8QdqxD_WyHlIcsykEO1FknqGAuWwaBAJmqMShjlAbYVMaK2xg1K-1zBEMBUEjM96x7yJb6ykezbVh46jSWRwwhKMblFY3Yx"
 //url the API is calling on to retrieve information
-var requestUrl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search";
+var requestUrl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=NYC";
 
 $.ajax({
     url: requestUrl,
@@ -10,12 +10,11 @@ $.ajax({
         "accept": "application/json",
         "x-requested-with": "xmlhttprequest",
         "Access-Control-Allow-Origin": "*",
-        "Authorization": "Bearer ${apiKey}"
-    },
-    data: {
-        location: "San Francisco"
+        "Authorization": "Bearer " + apiKey
     }
-}).then(function (res) {
-    var results = res.data;
-    console.log(results);
+    /*data: {
+        location: "San Francisco"
+    }*/
+}).then(function (response) {
+    console.log(response);
 });
