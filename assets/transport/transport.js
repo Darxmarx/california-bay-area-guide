55debc1d4de1ac9640c5d7308c1dcbed54adc1f3
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-10-12 22:41:44 -0700

    the api is working, tested and got a response 200 from BART, it display the time of the next 2 bart in that station

--- assets/transport/transport.js
+++ assets/transport/transport.js
@@ -1,24 +1,28 @@
 
 var station = $('#stName');
 
+function fetchBart (station) {
 
+    var apiKey = "MW9S-E7SL-26DU-VV8V&json=y";
+
+    // fetch("https://api.bart.gov/api/etd.aspx?cmd=etd&orig="+ station + "&key=" + apiKey)
+    fetch("https://api.bart.gov/api/etd.aspx?cmd=etd&orig=RICH&key=MW9S-E7SL-26DU-VV8V&json=y")
+
+    .then(function (response) {
+        console.log(response)
+        return response.json();
+      })
+    
+      .then(function (data) {
+        console.log("data",data)
+        
+      });
+      
+}
+
+fetchBart();
 
 // Create a select menu, so the user can chose what is the station they want, the reason we are creating in this way as the dart require the station name abreviation.
-
-// function fetchBart (station) {
-
-//     var apiKey = "MW9S-E7SL-26DU-VV8V&json=y";
-
-//     // fetch("https://api.bart.gov/api/etd.aspx?cmd=etd&orig="+ station + "&key=" + apiKey)
-//     fetch("https://api.bart.gov/api/etd.aspx?cmd=etd&orig=RICH&key=MW9S-E7SL-26DU-VV8V&json=y")
-
-//     .then((response) =>response.json())
-//     console.log(response)
-//     .then((data) => (data))
-
-// }
-
-// fetchBart();
 
 $( function() {
     var availableTags = [
@@ -73,66 +77,9 @@ $( function() {
         "West Dublin",
         "West Oakland"
     ];
-    $( "#tags" ).autocomplete({
+    $("#station").autocomplete({
       source: availableTags
     });
   } );
 
 
-// $(function() {
-//     var stationName = [
-//         "12th St. Oakland City Center",
-//         "16th St. Mission (SF)",
-//         "19th St. Oakland",
-//         "24th St. Mission (SF)",
-//         "Ashby (Berkeley)",
-//         "Antioch",
-//         "Balboa Park (SF)",
-//         "Bay Fair (San Leandro)",
-//         "Berryessa / North San Jose",
-//         "Castro Valley",
-//         "Civic Center (SF)",
-//         "Coliseum",
-//         "Colma",
-//         "Concord",
-//         "Daly City",
-//         "Downtown Berkeley",
-//         "Dublin/Pleasanton",
-//         "El Cerrito del Norte",
-//         "El Cerrito Plaza",
-//         "Embarcadero (SF)",
-//         "Fremont",
-//         "Fruitvale (Oakland)",
-//         "Glen Park (SF)",
-//         "Hayward",
-//         "Lafayette",
-//         "Lake Merritt (Oakland)",
-//         "MacArthur (Oakland)",
-//         "Millbrae",
-//         "Milpitas",
-//         "Montgomery St. (SF)",
-//         "North Berkeley",
-//         "North Concord/Martinez",
-//         "Oakland Int'l Airport",
-//         "Orinda",
-//         "Pittsburg/Bay Point",
-//         "Pittsburg Center",
-//         "Pleasant Hill",
-//         "Powell St. (SF)",
-//         "Richmond",
-//         "Rockridge (Oakland)",
-//         "San Bruno",
-//         "San Francisco Int'l Airport",
-//         "San Leandro",
-//         "South Hayward",
-//         "South San Francisco",
-//         "Union City",
-//         "Warm Springs/South Fremont",
-//         "Walnut Creek",
-//         "West Dublin",
-//         "West Oakland"
-// //     ];
-// //     $('#stName').autocomplete({
-// //       source: stationName
-// //     });
-// //   });
