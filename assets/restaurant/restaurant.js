0c72ec80ef8fd998ae32cc427dc51ec63d2202ef
Darx <mcrivabode@gmail.com>
2022-10-13 14:07:18 -0700

    add ajax, attempt cors-anywhere as fix

--- assets/restaurant/restaurant.js
+++ assets/restaurant/restaurant.js
@@ -1,27 +1,21 @@
 //personal API key, 5,000 API calls per day
 const apiKey = "3pOEjNFRg7sRIZM6MAdUVDCecyWU-vdaV8QdqxD_WyHlIcsykEO1FknqGAuWwaBAJmqMShjlAbYVMaK2xg1K-1zBEMBUEjM96x7yJb6ykezbVh46jSWRwwhKMblFY3Yx"
+//url the API is calling on to retrieve information
+var requestUrl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search";
 
-var searchRequest = { //restricts the restaurant search so that it's only locations in san francisco
-    location: "san francisco, ca"
-}
-
-var options = {
+$.ajax({
+    url: requestUrl,
     method: "GET",
     headers: {
-        "Content-Type": "application/json",
-        //authenticate API calls with the API key via setting Authorization HTTP header value as Bearer <yourapikey>
-        "Authorization": "Bearer " + apiKey
+        "accept": "application/json",
+        "x-requested-with": "xmlhttprequest",
+        "Access-Control-Allow-Origin": "*",
+        "Authorization": "Bearer ${apiKey}"
+    },
+    data: {
+        location: "San Francisco"
     }
-}
-
-function searchRestaurants() {
-    var apiUrl = "https://api.yelp.com/v3"
-
-    fetch(apiUrl, options)
-        .then(function (response) {
-            console.log("response", response);
-            return response.json();
-        })
-}
-
-searchRestaurants();
+}).then(function (res) {
+    var results = res.data;
+    console.log(results);
+});
