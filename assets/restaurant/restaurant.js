1ca489b2f0c558e48353ae82a036868e5e425c95
Darx <mcrivabode@gmail.com>
2022-10-14 09:30:49 -0700

    make note of request demo requirement, fix location

--- assets/restaurant/restaurant.js
+++ assets/restaurant/restaurant.js
@@ -1,7 +1,8 @@
 //personal API key, 5,000 API calls per day
 var apiKey = "3pOEjNFRg7sRIZM6MAdUVDCecyWU-vdaV8QdqxD_WyHlIcsykEO1FknqGAuWwaBAJmqMShjlAbYVMaK2xg1K-1zBEMBUEjM96x7yJb6ykezbVh46jSWRwwhKMblFY3Yx"
 //url the API is calling on to retrieve information
-var requestUrl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=NYC";
+    //https://cors-anywhere.herokuapp.com/ is added to serve as a proxy API to bypass CORS issues
+var requestUrl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=san-francisco";
 
 $.ajax({
     url: requestUrl,
@@ -10,11 +11,9 @@ $.ajax({
         "accept": "application/json",
         "x-requested-with": "xmlhttprequest",
         "Access-Control-Allow-Origin": "*",
-        "Authorization": "Bearer " + apiKey
+        //To authenticate API calls with the API Key, set the Authorization HTTP header value as Bearer API_KEY.
+        "Authorization": "Bearer " + apiKey 
     }
-    /*data: {
-        location: "San Francisco"
-    }*/
 }).then(function (response) {
     console.log(response);
 });
