b5eed0da959efd4a1c452ef019445ab6ab87a05f
Darx <mcrivabode@gmail.com>
2022-10-15 23:29:14 -0700

    limit restaurant list to 10, work on appending

--- assets/restaurant/restaurant.js
+++ assets/restaurant/restaurant.js
@@ -4,9 +4,11 @@ var apiKey = "3pOEjNFRg7sRIZM6MAdUVDCecyWU-vdaV8QdqxD_WyHlIcsykEO1FknqGAuWwaBAJm
     //https://cors-anywhere.herokuapp.com/ is added to serve as a proxy API to bypass CORS issues
 var requestUrl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search";
 
+var restaurantContainerEl = $("#restaurant-container");
 var searchTerm = $("#search-term");
 var searchBtn = $("#search-btn");
 
+//retrieves restaurant data from api
 function loadRestaurants() {
     $.ajax({
     url: requestUrl,
@@ -21,13 +23,32 @@ function loadRestaurants() {
     data: { //dynamically adds in parameters
         location: "san-francisco", //always searches the San Francisco area for results
         categories: "restaurants", //always searches restaurants
+        limit: 10, //returns 10 results for whatever user searched
         term: searchTerm.val() //plug in term from user input
     }
     }).then(function (response) {
         console.log(response);
+        displayRestaurants;
     });
 }
 
+//generates and appends elements based on data from loadRestaurants
+function displayRestaurants() {
+
+    for (var i = 0; i <= 9; i++) {
+        var restName = root.businesses[i].name;
+
+        var restEl = document.createElement("div");
+        var titleEl = document.createElement("span");
+        titleEl.textContent = restName;
+
+        restEl.appendChild(titleEl);
+    }
+
+    restaurantContainerEl.appendChild(restEl);
+}
+
+//modal that requires user to request access to cors-anywhere.herokuapp demo server
 function createModal() {
     $('#dialog').dialog();
 }
