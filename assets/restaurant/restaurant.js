8da604354ec504aa01f634223fcf156a977ebeb6
Darx <mcrivabode@gmail.com>
2022-10-16 18:12:54 -0700

    fix images as best we can, remove dummy font links, prep for fixing navbar

--- assets/restaurant/restaurant.js
+++ assets/restaurant/restaurant.js
@@ -41,14 +41,12 @@ function loadRestaurants() {
             console.log(restUrl);   
             
             $("#rest-name-" + i).html(restName);
-            $("#rest-img-" + i).html("<a href=" + restUrl + "><img src=" + restImg + " alt='restaurant image'/></a>");
+            $("#rest-img-" + i).html("<a href=" + restUrl + "><img class='flex-none' src=" + restImg + " alt='restaurant image'/></a>");
             //$("#rest-url-" + i).html("href=" + restUrl);
         }
     })
     restGrid.removeClass("invisible");
 }
-
-
 
 //modal that requires user to request access to cors-anywhere.herokuapp demo server
 function createModal() {
